2f559591da7319248f838bf7e6b07492172f390d
TotallyNotErik <Choverik@gmail.com>
2023-10-18 18:49:04 -0700

    Started State Machine

---
+++ UFG/Assets/Scripts/Idle.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Idle : Grounded
+{
+    protected override void OnEnter()
+    {
+        //Play idle animation
+    }
+
+    protected override void OnUpdate()
+    {
+        //Down
+            //Crouch
+
+        //Forward
+            //Walk
+
+        //Back
+            //WalkBackwards?
+    }
+}
