b7940503e9057a54fa96ef4ac934fc45fe88d9f2
Erik Chov <ChovErik@Gmail.com>
2023-11-20 13:44:51 -0700

    Pushed web build

--- UFG/Assets/Scripts/StartMenu.cs
+++ UFG/Assets/Scripts/StartMenu.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
-using UnityEditor.SceneManagement;
+//using UnityEditor.SceneManagement;
 using UnityEngine.SceneManagement;
 
 public class StartMenu : MonoBehaviour
